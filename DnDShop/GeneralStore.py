b2356789800ae04221c66ccfaa750dc05a2b1656
Pollyanna584 <monk.phillip@gmail.com>
2023-07-23 16:39:17 -0700

    script now working with variable

--- DnDShop/GeneralStore.py
+++ DnDShop/GeneralStore.py
@@ -2,28 +2,33 @@
 
 ## Libraries
 import pandas as pd
+import random as rd
+import os
 
 ## Pandas options
 pd.options.display.max_rows = 9999 #Max rows shown
+dir = os.path.dirname(os.path.abspath(__file__))
 
 ## Import CSVs
-m_combat = pd.read_csv(r'/Users/pmonk/GitHub/PersonalRepo/Python/DnDShop/Items/MagicItems/combatitems.csv')
-m_noncombat = pd.read_csv(r'/Users/pmonk/GitHub/PersonalRepo/Python/DnDShop/Items/MagicItems/noncombatitems.csv')
-m_consume = pd.read_csv(r'/Users/pmonk/GitHub/PersonalRepo/Python/DnDShop/Items/MagicItems/consumables.csv')
-n_gear = pd.read_csv(r'/Users/pmonk/GitHub/PersonalRepo/Python/DnDShop/Items/NormalItems/adventuregear.csv')
-n_armor = pd.read_csv(r'/Users/pmonk/GitHub/PersonalRepo/Python/DnDShop/Items/NormalItems/armor.csv')
-n_weapon = pd.read_csv(r'/Users/pmonk/GitHub/PersonalRepo/Python/DnDShop/Items/NormalItems/weapons.csv')
-n_tools = pd.read_csv(r'/Users/pmonk/GitHub/PersonalRepo/Python/DnDShop/Items/NormalItems/tools.csv')
+m_combat = pd.read_csv(f'{dir}/Items/MagicItems/combatitems.csv')
+m_noncombat = pd.read_csv(f'{dir}/Items/MagicItems/noncombatitems.csv')
+m_consume = pd.read_csv(f'{dir}/Items/MagicItems/consumables.csv')
+n_gear = pd.read_csv(f'{dir}/Items/NormalItems/adventuregear.csv') 
+n_armor = pd.read_csv(f'{dir}/Items/NormalItems/armor.csv')
+n_weapon = pd.read_csv(f'{dir}/Items/NormalItems/weapons.csv')
+n_tools = pd.read_csv(f'{dir}/Items/NormalItems/tools.csv')
 
-volume_choice = input("Choose an option:\n1 - lots of items\n2 - normal amount\n3 - few items\n")
-
-global item_volume
-if volume_choice=="1":
-    print(m_combat.sample(frac=.06))
-elif volume_choice=="2":
-    print(m_combat.sample(frac=.03))
-elif volume_choice=="3":
-    print(m_combat.sample(frac=.015))
-else:
-    print("nope")
-    exit
+## Item Randomizer
+def rollItems(item_count, shop_count, row):
+    global shop_inventory
+    shop_array=range(0,item_count)
+    shop_inventory=rd.sample(shop_array,shop_count)
+    
+## Combat Items
+m_combat_count = len(m_combat)
+m_combat_shop_count = round(m_combat_count*.03)
+newrow=0
+rollItems(m_combat_count, m_combat_shop_count, newrow)
+m_combat_inventory = shop_inventory
+m_combat_shop = m_combat.loc[m_combat_inventory, ["Name", "Price"]]
+print(m_combat_shop)
